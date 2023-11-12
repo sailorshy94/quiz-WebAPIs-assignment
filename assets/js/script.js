e8495521de585615e98b771e4160116cc274112f
sailorshy94 <sbutle94@gmail.com>
2023-11-12 00:53:16 -0500

    added functions to allow toggling between different pages, added init function for when webpage is first accessed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,40 @@
-function showBeginPage {
+var startPage = document.querySelector("#start");
+var quizPage = document.querySelector("#quiz");
+var completePage = document.querySelector("#complete");
+var scoresPage = document.querySelector("#scores");
 
+
+function showStartPage() {
+    startPage.style.display = null;
+    quizPage.style.display = "none";
+    completePage.style.display = "none";
+    scoresPage.style.display = "none";
 }
 
-function showQuizPage {
-
+function showQuizPage() {
+    startPage.style.display = "none";
+    quizPage.style.display = null;
+    completePage.style.display = "none";
+    scoresPage.style.display = "none";
 }
 
-function showCompletePage {
-
+function showCompletePage() {
+    startPage.style.display = "none";
+    quizPage.style.display = "none";
+    completePage.style.display = null;
+    scoresPage.style.display = "none";
 }
+
+function showScoresPage() {
+    startPage.style.display = "none";
+    quizPage.style.display = "none";
+    completePage.style.display = "none";
+    scoresPage.style.display = null;
+}
+
+function init() {
+    showStartPage();
+}
+
+init();
 
